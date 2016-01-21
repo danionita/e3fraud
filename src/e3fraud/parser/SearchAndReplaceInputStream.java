395b41e987d4ca8b1c16e6c6cdf1703389b467ee
danionita <Dan@130.89.14.189>
2016-01-21 11:09:52 +0100

    added updated sample scenarios

--- src/e3fraud/parser/SearchAndReplaceInputStream.java
+++ src/e3fraud/parser/SearchAndReplaceInputStream.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package rdfparser.parser;
+package e3fraud.parser;
 
 import java.io.IOException;
 import java.io.InputStream;
